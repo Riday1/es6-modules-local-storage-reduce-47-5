02911e15e2bf0aa31579be8a17d4f530118b8ee1
Riday1 <rrriday100@gmail.com>
2022-11-04 00:45:21 +0600

    added event handler parameter using annonymous function , and stored product details into cart using localStorage in fakeDb.js file

--- src/Cosmetics/FakeDB/FakeDB.js
+++ src/Cosmetics/FakeDB/FakeDB.js
@@ -7,19 +7,13 @@ const addToDb = (id) => {
     else {
         shoppingCart = {};
     }
-    // localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
-    console.log(shoppingCart)
-
-
-    // localStorage.setItem(id, 1)
+    // console.log(shoppingCart)
     const quantity = shoppingCart[id];
     if (quantity) {
         const newQuantity = quantity + 1;
         shoppingCart[id] = newQuantity;
-        // localStorage.setItem(id, newQuantity)
     }
     else {
-        // localStorage.setItem(id, 1);
         shoppingCart[id] = 1;
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
